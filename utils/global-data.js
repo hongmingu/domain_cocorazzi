1c5ce9fdac2928ece04dcda5b1932406b14f392d
hongmingu <gmh397@nate.com>
2023-07-06 21:57:12 +0900

    changed default

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : '외모, 보컬, 예체능 정보| Be great girl';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '기획사, 연예인, 가수, 예체능, 연예기획사 계열의 정보를 제공합니다.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
